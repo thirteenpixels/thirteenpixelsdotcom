0ddd877c51e49cdcb9d6b11042410e61fd68711e
Bill Evans <thirteenpixels@users.noreply.github.com>
2023-07-05 12:47:26 -0400

    converting site to new tailwind template and updated version of next

---
+++ src/pages/about.jsx
@@ -0,0 +1,94 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import Link from 'next/link'
+import clsx from 'clsx'
+
+import { Container } from '@/components/Container'
+import {
+  GitHubIcon,
+  InstagramIcon,
+  LinkedInIcon,
+  TwitterIcon,
+} from '@/components/SocialIcons'
+import portraitImage from '@/images/portrait.jpg'
+
+function SocialLink({ className, href, children, icon: Icon }) {
+  return (
+    <li className={clsx(className, 'flex')}>
+      <Link
+        href={href}
+        className="group flex text-sm font-medium text-zinc-800 transition hover:text-teal-500 dark:text-zinc-200 dark:hover:text-teal-500"
+      >
+        <Icon className="h-6 w-6 flex-none fill-zinc-500 transition group-hover:fill-teal-500" />
+        <span className="ml-4">{children}</span>
+      </Link>
+    </li>
+  )
+}
+
+function MailIcon(props) {
+  return (
+    <svg viewBox="0 0 24 24" aria-hidden="true" {...props}>
+      <path
+        fillRule="evenodd"
+        d="M6 5a3 3 0 0 0-3 3v8a3 3 0 0 0 3 3h12a3 3 0 0 0 3-3V8a3 3 0 0 0-3-3H6Zm.245 2.187a.75.75 0 0 0-.99 1.126l6.25 5.5a.75.75 0 0 0 .99 0l6.25-5.5a.75.75 0 0 0-.99-1.126L12 12.251 6.245 7.187Z"
+      />
+    </svg>
+  )
+}
+
+export default function About() {
+  return (
+    <>
+      <Head>
+        <title>About - Bill Evans</title>
+        <meta
+          name="description"
+          content="I'm Bill Evans. I'm a programmer, marketer and entrepeneur."
+        />
+      </Head>
+      <Container className="mt-16 sm:mt-32">
+        <div className="grid grid-cols-1 gap-y-16 lg:grid-cols-2 lg:grid-rows-[auto_1fr] lg:gap-y-12">
+          <div className="lg:pl-20">
+            <div className="max-w-xs px-2.5 lg:max-w-none">
+              <Image
+                src={portraitImage}
+                alt=""
+                sizes="(min-width: 1024px) 32rem, 20rem"
+                className="aspect-square rounded-2xl bg-zinc-100 object-cover dark:bg-zinc-800"
+              />
+            </div>
+          </div>
+          <div className="lg:order-first lg:row-span-2">
+            <h1 className="text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
+            👋 Hi there, I&rsquo;m Bill Evans.
+            </h1>
+            <div className="mt-6 space-y-7 text-base text-zinc-600 dark:text-zinc-400">
+              <p>I started my career wanting to make video games. I still love playing games and always have an idea for a new game rattling around in my head. When I first started programming, the only way to make games was by learning C and Assembly and diving in. I made my best attempts, but it became clear early on that this wouldn&rsquo;t be my&nbsp;path.</p>
+              <p>I did enjoy programming, but thankfully, the internet gave me an outlet for that passion. I&rsquo;ve been very fortunate in my career and worked with amazing people on amazing projects. I even got to make a few&nbsp;games.</p>
+              <p>Today I&rsquo;m one of the founders of <a href="https://tsyklonstudio.com" target="_blank" class="underline">Tsyklon</a>. We build wonderful emails for companies that I&rsquo;m sure you&rsquo;ve heard of, but you can read more about that on our&nbsp;<a href="https://tsyklonstudio.com" target="_blank" class="underline">website</a>.</p>
+              <p>Next year I will have been on this journey for a quarter of a century. If I&rsquo;m lucky, I have another twenty-five ahead of me. In the meantime, I will do my best to enjoy&nbsp;it.</p>
+            </div>
+          </div>
+          <div className="lg:pl-20">
+            <ul role="list">
+              <SocialLink href="#" icon={GitHubIcon} className="mt-4">
+                Follow on GitHub
+              </SocialLink>
+              <SocialLink href="#" icon={LinkedInIcon} className="mt-4">
+                Follow on LinkedIn
+              </SocialLink>
+              <SocialLink
+                href="mailto:bill@thirteenpixels.com"
+                icon={MailIcon}
+                className="mt-8 border-t border-zinc-100 pt-8 dark:border-zinc-700/40"
+              >
+                bill@thirteenpixels.com
+              </SocialLink>
+            </ul>
+          </div>
+        </div>
+      </Container>
+    </>
+  )
+}
